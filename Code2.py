196cbaac7e031d3304bf910933818bb0e71661d1
schmieol <schmieol@katharineum.de>
2026-07-02 12:55:23 +0200

    Change file uploader prompt to 'Choose an image'

--- Code2.py
+++ Code2.py
@@ -44,7 +44,7 @@ except Exception as e:
 # ==========================================
 
 uploaded_file = st.file_uploader(
-    "Choose an image or video...",
+    "Choose an image",
     type=["jpg", "jpeg", "png", "mp4", "avi"]
 )
 
